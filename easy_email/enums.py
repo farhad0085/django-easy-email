0fd37ec3581ab64aa33007319bd0db50424871d9
Farhad Hossain <farhadhossain0085@gmail.com>
2025-03-15 03:48:16 +0600

    added BaseEnum

--- easy_email/enums.py
+++ easy_email/enums.py
@@ -11,6 +11,10 @@ class NoModificationMeta(type):
         raise ValueError(f"Cannot delete {name}")
 
 
-class EmailStatus(metaclass=NoModificationMeta):
+class BaseEnum(metaclass=NoModificationMeta):
+    """Inherit this class for creating enum in the system"""
+
+
+class EmailStatus(BaseEnum):
     SUCCESS = 1
     ERROR = 2
